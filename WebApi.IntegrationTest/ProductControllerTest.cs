207e08db80da466e35f414365b729eec915f7034
Ricardo <ricardo9300@gmail.com>
2018-06-27 00:14:43 +0100

    Doing...

--- WebApi.IntegrationTest/ProductControllerTest.cs
+++ WebApi.IntegrationTest/ProductControllerTest.cs
@@ -12,36 +12,16 @@ namespace WebApi.IntegrationTest
     {
 
         [Fact]
-        public async Task TestCount()
+        public async Task Test_Count()
         {
-            using (var client = new ClienteProvider().Client)
-            {
-                var response = await client.GetAsync("/api/Product/Count");
-
-                response.EnsureSuccessStatusCode();
-
-                Assert.Equal(HttpStatusCode.OK, response.StatusCode);
-
-                //Chech the cound
-            }
+            //Check result and responses....
 
         }
 
         [Fact]
         public async Task Test_Post()
         {
-            using (var client = new ClienteProvider().Client)
-            {
-                var response = await client.PostAsync("/api/Product/Post"
-                    , new StringContent(JsonConvert.SerializeObject(new Product() { Id= 1, Description= "Test"}), Encoding.UTF8, "application/json" )
-                    );
-
-                response.EnsureSuccessStatusCode();
-
-                Assert.Equal(HttpStatusCode.OK, response.StatusCode);
-
-                //Check when the model is wrong...
-            }
+            //Assert.
         }
     }
 }
