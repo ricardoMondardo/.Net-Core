da48bd92a1c7ea86980b486fd0c3f6691bd5759f
ricardoMondardo <ricardo9300@gmail.com>
2019-01-07 13:03:35 -0200

    add email Manager
    add email Manager

---
+++ Web.EmailManager/Interfaces/IEmailService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Web.EmailManager.Models;
+
+namespace Web.EmailManager.Interfaces
+{
+    public interface IEmailService
+    {
+        void Send(EmailMessage emailMessage);
+        List<EmailMessage> ReceiveEmail(int maxCount = 10);
+    }
+}
---
+++ Web.EmailManager/Models/responseDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.EmailManager.Models
+{
+    public class responseDTO
+    {
+        public string msg { get; set; }
+    }
+}
