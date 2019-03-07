8f5d7d23d7b5f758315d19aaa31dc30efa03eb03
ricardoMondardo <ricardo9300@gmail.com>
2019-03-07 19:11:04 -0300

    ...
    ...

--- Web.Server/Controllers/ApiProductController.cs
+++ Web.Server/Controllers/ApiProductController.cs
@@ -11,7 +11,7 @@ namespace Web.Server.Controllers
 {
     [Produces("application/json")]
     [Route("api/resources/")]
-    //[Authorize]
+    [Authorize]
     public class ApiProductController : _BasePaggingController<ProductDTO>
     {
         private IProductService _productService;
