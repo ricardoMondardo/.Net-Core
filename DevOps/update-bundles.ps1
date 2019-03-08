7573747e0ac09dfd63fa16931eb67436e32b04c0
ricardoMondardo <ricardo9300@gmail.com>
2019-03-08 19:07:06 -0300

    ....
    ....

--- DevOps/update-bundles.ps1
+++ DevOps/update-bundles.ps1
@@ -1,7 +1,3 @@
-cd C:\projects\Search.vuej
-
-npm run build:prod
-
 Remove-Item C:\projects\Solution.Net.Core\Web.Server\wwwroot\js\*.*
 
 Copy-Item -Path C:\projects\Search.vuej\build\*.js -Destination C:\projects\Solution.Net.Core\Web.Server\wwwroot\js -Force
